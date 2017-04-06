35b1dc88da1eaf5f543a2767b6ff46b4e94ed3fe
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-04-06 14:40:12 +0200

    base server config

--- server/index.js
+++ server/index.js
@@ -1,62 +1,63 @@
-const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
-var app = express();
+// charge le module express (web framework)
+const fs = require('fs');
+// charge le module de systeme de fichiers
 
-const config = { port: 9226 };
+var app = express();
+// The app object conventionally denotes the Express application
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 app.use(express.static(__dirname + '/../client'));
-
 
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
-const DATABASE_NAME = 'geoData.db';
+// app.get('/patinoires', function(req, res) {
 
-app.get('/patinoires', function(req, res) {
+//     var db = new sqlite3.Database(DATABASE_NAME);
 
-    var db = new sqlite3.Database(DATABASE_NAME);
+//     db.serialize(function() {
 
-    db.serialize(function() {
+//         db.all("SELECT * FROM patinoires", function(err, rows) {
 
-        db.all("SELECT * FROM patinoires", function(err, rows) {
+//             for (var i = 0; i < rows.length; ++i) {
+//                 rows[i].coordinates = [rows[i].lng, rows[i].lat];
 
-            for (var i = 0; i < rows.length; ++i) {
-                rows[i].coordinates = [rows[i].lng, rows[i].lat];
+//                 delete rows[i].lng;
+//                 delete rows[i].lat;
+//             }
+//             console.log(rows);
+//             res.send(rows);
+//         });
 
-                delete rows[i].lng;
-                delete rows[i].lat;
-            }
-            console.log(rows);
-            res.send(rows);
-        });
+//     });
 
-    });
+//     db.close();
+// });
 
-    db.close();
-});
+// app.get('/boulodromes', function(req, res) {
 
-app.get('/boulodromes', function(req, res) {
+//     var db = new sqlite3.Database(DATABASE_NAME);
 
-    var db = new sqlite3.Database(DATABASE_NAME);
+//     db.serialize(function() {
 
-    db.serialize(function() {
+//         db.all("SELECT * FROM boulodromes", function(err, rows) {
 
-        db.all("SELECT * FROM boulodromes", function(err, rows) {
+//             for (var i = 0; i < rows.length; ++i) {
+//                 rows[i].coordinates = [rows[i].lng, rows[i].lat];
 
-            for (var i = 0; i < rows.length; ++i) {
-                rows[i].coordinates = [rows[i].lng, rows[i].lat];
+//                 delete rows[i].lng;
+//                 delete rows[i].lat;
+//             }
+//             res.send(rows);
+//         });
 
-                delete rows[i].lng;
-                delete rows[i].lat;
-            }
-            res.send(rows);
-        });
+//     });
 
-    });
-
-    db.close();
-});
+//     db.close();
+// });
 
 app.listen(config.port, function() {
     console.log('listening on *:' + config.port);
