c059306594e62555cb8cd76c8a88da1e8815ae7e
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-04-10 11:18:43 +0200

    use a class to represent an OSRM request

--- server/osrm.js
+++ server/osrm.js
@@ -4,134 +4,84 @@ const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 const request = require('request');
 
-/**
- * Make the request Url for osrm-routed (HTTP server)
- *
- * @param {object} osrmReq - the object with the parameters of the request, see the osrmReq object created below
- *
- * @returns {string} the url for the request ready to be sent.
- */
-function makeUrl(osrmReq) {
+class OsrmRequest {
+  constructor() {
+    // One of the following values: route, nearest, table, match, trip, tile
+    this.service = "trip";
 
-  let requestUrl = config.osrm.protocol + '://' + config.osrm.ip + ':' + config.osrm.port + '/' +
-    osrmReq.service + '/' + osrmReq.version + '/' + osrmReq.profile + '/';
+    // Version of the protocol implemented by the service. v1 for all OSRM 5.x installations
+    this.version = "v1";
 
-  for (let i = 0; i < osrmReq.coords.length; ++i) {
+    // Mode of transportation, is determined statically by the Lua profile that is used to prepare the data using osrm-extract. Typically car, bike or foot if using one of the supplied profiles.
+    this.profile = "car";
 
-    // requestUrl += osrmReq.coords[i].lo + ',' + osrmReq.coords[i].la + ';';
-    requestUrl += osrmReq.coords[i][1] + ',' + osrmReq.coords[i][0] + ';';
+
+    // an array of coordinates that are arrays of 2 elements : [latitude, longitude]
+    this.coords = [];
+
+    /*
+        steps : Return route steps for each route leg
+
+        geometries : Returned route geometry format (influences overview and per step)
+
+        roundtrip : Return route is a roundtrip
+        source : Return route starts at `any` or `first` coordinate
+        destination : Return route ends at `any` or `last` coordinate
+        if source=any&destination=any the returned round-trip will still start at the first input coordinate by default.
+
+        overview : Add overview geometry either `full`, `simplified` according to highest zoom level it could be display on, or not at all (`false`).
+    */
+    this.options = {
+      steps: false,
+      geometries: "geojson", // Returned route geometry in Geojson for Leaflet
+      overview: "full" // full overview geometry
+    };
   }
 
-  // removing the last ';'
-  requestUrl = requestUrl.slice(0, -1);
+  /**
+   * Make the request Url for osrm-routed (HTTP server)
+   *
+   * @returns {string} the url for the request ready to be sent.
+   */
+  makeUrl() {
+    let requestUrl = config.osrm.protocol + '://' + config.osrm.ip + ':' + config.osrm.port + '/' +
+      this.service + '/' + this.version + '/' + this.profile + '/';
 
-  // get the array of options keys
-  let optionsKeys = Object.keys(osrmReq.options);
+    for (let i = 0; i < this.coords.length; ++i) {
 
-  if (optionsKeys.length > 0) {
-    requestUrl += '?';
-
-    for (let i = 0; i < optionsKeys.length; ++i) {
-
-      let key = optionsKeys[i];
-      requestUrl += key + '=' + osrmReq.options[key] + '&';
+      requestUrl += this.coords[i][1] + ',' + this.coords[i][0] + ';';
     }
 
-    // removing the last '&'
+    // removing the last ';'
     requestUrl = requestUrl.slice(0, -1);
+
+    // get the array of options keys
+    let optionsKeys = Object.keys(this.options);
+
+    if (optionsKeys.length > 0) {
+      requestUrl += '?';
+
+      for (let i = 0; i < optionsKeys.length; ++i) {
+
+        let key = optionsKeys[i];
+        requestUrl += key + '=' + this.options[key] + '&';
+      }
+
+      // removing the last '&'
+      requestUrl = requestUrl.slice(0, -1);
+    }
+
+    return requestUrl;
   }
 
-  return requestUrl;
+  addCoords(lat, lng) {
+
+    // doit rajouter un tableau de la forme [latitute, longitude] dans coords
+
+    this.coords.push([lat, lng]);
+  }
+
 }
-
-
-let osrmReq = {
-
-  // One of the following values: route, nearest, table, match, trip, tile
-  service: "trip",
-
-  // Version of the protocol implemented by the service. v1 for all OSRM 5.x installations
-  version: "v1",
-
-  // Mode of transportation, is determined statically by the Lua profile that is used to prepare the data using osrm-extract. Typically car, bike or foot if using one of the supplied profiles.
-  profile: "car",
-
-
-  // an array of coordinates that are arrays of 2 elements : [latitude, longitude]
-  coords: [
-    [
-      43.91681, 2.1382 // Ateliers Municipaux Charcot
-    ],
-    [
-      43.92712, 2.14642 // Hôtel de Ville
-    ],
-    [
-      43.9409, 2.1185 // Golf de Lasbordes
-    ],
-    [
-      43.9176354, 2.1101724 // Buffalo Grill
-    ],
-    [
-      43.92501, 2.14969 // CCAS
-    ],
-    [
-      43.9216692, 2.1660809 // Stadium
-    ],
-    [
-      43.9536903, 2.1717809 // Mairie de Lescure-d'Albigeois
-    ],
-    [
-      43.92956, 2.16142 // random address
-    ],
-    [
-      43.94173, 2.1456 // random address
-    ],
-    [
-      43.94494, 2.13718 // random address
-    ],
-    [
-      43.90874, 2.11212 // Mairie du Sequestre
-    ],
-    [
-      43.92739, 2.10972 // random address
-    ],
-    [
-      43.93777, 2.08174 // random address
-    ],
-    [
-      43.94865, 2.211 // random address
-    ],
-    [
-      43.92801, 2.22164 // random address
-    ],
-    [
-      43.91255, 2.21254 // random address
-    ],
-    [
-      43.89177, 2.17134 // random address
-    ]
-  ],
-
-  /*
-      steps : Return route steps for each route leg
-
-      geometries : Returned route geometry format (influences overview and per step)
-
-      roundtrip : Return route is a roundtrip
-      source : Return route starts at `any` or `first` coordinate
-      destination : Return route ends at `any` or `last` coordinate
-      if source=any&destination=any the returned round-trip will still start at the first input coordinate by default.
-
-      overview : Add overview geometry either `full`, `simplified` according to highest zoom level it could be display on, or not at all (`false`).
-  */
-  options: {
-    steps: false,
-    geometries: "geojson", // Returned route geometry in Geojson for Leaflet
-    overview: "full" // full overview geometry
-  }
-
-};
-
 
 /**
  *
@@ -140,7 +90,15 @@ let osrmReq = {
  */
 function getTrip() {
 
-  let madeUrl = makeUrl(osrmReq);
+  let oReq = new OsrmRequest();
+
+  oReq.addCoords(43,2);
+  oReq.addCoords(43.1,2.1);
+  oReq.addCoords(43.2,2.2);
+
+  console.log(oReq);
+
+  let madeUrl = oReq.makeUrl();
 
   console.log(madeUrl);
 
