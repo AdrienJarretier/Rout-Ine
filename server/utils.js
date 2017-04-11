3b6d0150c618c52d4b0f06bb24d62a5401817ecc
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-04-11 14:21:14 +0200

    function to separe an array into balanced chunks

---
+++ server/utils.js
@@ -0,0 +1,27 @@
+function chunkify(array, nbChunks) {
+
+  let chunkified = [];
+
+  // let minChunkLength;
+
+  let sliceStart = 0;
+
+  let remainingElements = array.length;
+
+  for (let i = 0; i < nbChunks; ++i) {
+
+    let chunkSize = Math.floor(remainingElements / (nbChunks - i));
+    let sliceEnd = sliceStart + chunkSize;
+
+    chunkified.push(array.slice(sliceStart, sliceEnd));
+
+    remainingElements -= chunkSize;
+
+    sliceStart = sliceEnd;
+  }
+
+  return chunkified;
+
+}
+
+exports.chunkify = chunkify;
