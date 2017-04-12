333185623af69b97b0a3b77d3a02056d488aacef
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-04-12 11:23:41 +0200

    new function to get the opposite of a color in utils

--- client/utils.js
+++ client/utils.js
@@ -36,5 +36,24 @@ function randomColor() {
 }
 exports.randomColor = randomColor;
 
+function negateColor(htmlColorString) {
+
+  let r = Number.parseInt(htmlColorString.slice(1, 3), 16);
+  let g = Number.parseInt(htmlColorString.slice(3, 5), 16);
+  let b = Number.parseInt(htmlColorString.slice(5, 7), 16);
+
+  r = 255 - r;
+  g = 255 - g;
+  b = 255 - b;
+
+  let nR = r.toString(16);
+  let nG = g.toString(16);
+  let nB = b.toString(16);
+
+  return '#' + nR + nG + nB;
+
+}
+exports.negateColor = negateColor;
+
 },{}]},{},[1])(1)
 });
--- server/utils.js
+++ server/utils.js
@@ -34,3 +34,22 @@ function randomColor() {
   return color;
 }
 exports.randomColor = randomColor;
+
+function negateColor(htmlColorString) {
+
+  let r = Number.parseInt(htmlColorString.slice(1, 3), 16);
+  let g = Number.parseInt(htmlColorString.slice(3, 5), 16);
+  let b = Number.parseInt(htmlColorString.slice(5, 7), 16);
+
+  r = 255 - r;
+  g = 255 - g;
+  b = 255 - b;
+
+  let nR = r.toString(16);
+  let nG = g.toString(16);
+  let nB = b.toString(16);
+
+  return '#' + nR + nG + nB;
+
+}
+exports.negateColor = negateColor;
