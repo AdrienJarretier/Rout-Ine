559446c88fe76ec3594fc04d807395245ac2a54f
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-05-03 11:14:14 +0200

    using function extractNamesList

--- server/testTournee.js
+++ server/testTournee.js
@@ -12,6 +12,8 @@ const request = require('request');
 
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
+getAll();
+
 function getAll() {
 
   return new Promise((resolve, reject) => {
@@ -45,13 +47,6 @@ function getAll() {
 
 }
 
-extractNamesList('tour1and2.csv')
-  .then((names) => {
-
-    console.log(names);
-
-  });
-
 function extractNamesList(csvFile) {
 
   return new Promise((resolve, reject) => {
@@ -79,33 +74,6 @@ function get(tourFile) {
 
   return new Promise((resolve, reject) => {
 
-    const csvFile = fs.openSync(tourFile, 'r');
-
-    const csvContent = fs.readFileSync(csvFile);
-
-    fs.closeSync(csvFile);
-
-    var dataArray = csvParse(csvContent, { delimiter: "," });
-
-    // let namesPlaceholder = '';
-    // let names = [];
-
-    // for (let line of dataArray) {
-    //   namesPlaceholder += '?,';
-    //   names.push(line[0]);
-    // }
-
-    // namesPlaceholder = namesPlaceholder.slice(0, -1);
-
-    // const sql = ' SELECT distinct a.id, a.label, a.town, a.lat, a.lng \n' +
-    //   ' FROM address a \n' +
-    //   ' RIGHT JOIN beneficiary ON a.id=beneficiary.address_id \n' +
-    //   ' WHERE a.id IS NOT NULL \n' +
-    //   ' AND beneficiary.name in (' + namesPlaceholder + ');';
-
-    // const select = mysql.format(sql, names);
-
-
     const sql =
       ' SELECT distinct a.id, a.label, a.town, a.lat, a.lng, beneficiary.id as beneficiary_id, name, birthdate, address_additional  \n' +
       ' FROM address a \n' +
@@ -113,167 +81,172 @@ function get(tourFile) {
       ' WHERE a.id IS NOT NULL \n' +
       ' AND beneficiary.name like ?;';
 
-    let dbCon = mysql.createConnection(config.db);
+    extractNamesList(tourFile)
+      .then((names) => {
 
-    let queriesDone = 0;
-    let notFound = 0;
+        let dbCon = mysql.createConnection(config.db);
 
-    let addresses = [];
+        let queriesDone = 0;
+        let notFound = 0;
 
-    console.log(dataArray.length + ' beneficiaries');
+        let addresses = [];
 
-    // la variable i est simplement utilisee pour garantir le maintient de lo'rdre initial des adresses
-    // on utilise alors une variable 'offset' pour eviter les cases vides quand l'adresses est deja dans notre tableau
-    let addressesOffset = 0;
-    for (let i in dataArray) {
+        console.log(names.length + ' beneficiaries');
 
-      let line = dataArray[i];
-      let name = line[0];
+        // la variable i est simplement utilisee pour garantir le maintient de lo'rdre initial des adresses
+        // on utilise alors une variable 'offset' pour eviter les cases vides quand l'adresses est deja dans notre tableau
+        let addressesOffset = 0;
+        for (let i in names) {
 
-      const select = mysql.format(sql, [name]);
+          let name = names[i];
 
-      dbCon.query(
-        select,
-        (err, rows, fields) => {
+          const select = mysql.format(sql, [name]);
 
-          // console.log(rows);
+          dbCon.query(
+            select,
+            (err, rows, fields) => {
 
-          if (err) throw err
+              // console.log(rows);
 
-          if (rows.length == 0) {
-            console.log('name not found : ' + name);
-            ++notFound;
-          } else if (notFound == 0) {
+              if (err) throw err
 
-            let notInArray = true;
+              if (rows.length == 0) {
+                console.log('name not found : ' + name);
+                ++notFound;
+              } else if (notFound == 0) {
 
-            // console.log('rows[0]');
-            // console.log(rows[0]);
+                let notInArray = true;
 
-            for (let ad of addresses) {
-              // console.log('ad');
-              // console.log(addresses);
+                // console.log('rows[0]');
+                // console.log(rows[0]);
 
-              if (ad.id == rows[0].id) {
+                for (let ad of addresses) {
+                  // console.log('ad');
+                  // console.log(addresses);
 
-                ad.addBeneficiary({
-                  name: rows[0].name,
-                  birthdate: rows[0].birthdate,
-                  address_additional: rows[0].address_additional
-                });
+                  if (ad.id == rows[0].id) {
 
-                notInArray = false;
-                ++addressesOffset;
-                break;
-              }
-            }
-
-            if (notInArray) {
-              addresses[i - addressesOffset] = new AddressFeature(rows[0]);
-
-              addresses[i - addressesOffset].addBeneficiary({
-                name: rows[0].name,
-                birthdate: rows[0].birthdate,
-                address_additional: rows[0].address_additional
-              });
-            }
-          }
-
-          if (++queriesDone == dataArray.length) {
-            dbCon.end();
-
-            db.ccasAddress()
-              .then((ccasAddress) => {
-
-                addresses.unshift(new AddressFeature(ccasAddress));
-
-                console.log('');
-                console.log(addresses.length + ' addresses');
-                // console.log(addresses);
-
-                addresses = new FeatureCollection(addresses);
-
-                // for(let adF of addresses.features)
-                //   console.log(adF.properties);
-
-                if (notFound == 0) {
-
-                  let testTrips = {
-                    original: {},
-                    osrmTrip: {},
-                    filled: 0,
-                    addresses: addresses
-                  };
-
-                  function requestToOsrm(service) {
-                    let oReq = new osrm.OsrmRequest(service, true);
-
-                    oReq.setFromAddresses(addresses);
-
-                    let madeUrl = oReq.makeUrl();
-
-
-                    request(madeUrl, (error, response, body) => {
-
-                      if (error) {
-                        console.log('error:', error); // Print the error if one occurred
-                        console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
-                      } else {
-
-                        // console.log('response from ' + service + ' service');
-                        let parsedBody = JSON.parse(body);
-
-                        // l'objet route retourne par osrm, nom different selon le service
-                        let route = {};
-
-                        if (service == 'route') {
-                          testTrips.original = parsedBody;
-                          route = parsedBody.routes[0];
-
-                        } else if (service == 'trip') {
-
-                          testTrips.osrmTrip = parsedBody;
-                          route = parsedBody.trips[0];
-                        }
-
-                        if (++testTrips.filled == 2) {
-
-                          resolve(testTrips);
-                        }
-
-                        console.log('');
-                        console.log('** ' + service + ' service **');
-                        console.log('distance : ' + Math.ceil(route.distance / 10) / 100 +
-                          ' km');
-
-                        let h = Math.floor(route.duration / 3600);
-                        let m = Math.ceil((route.duration % 3600) / 60);
-                        console.log('duration : ' + h + 'h ' + m);
-
-
-                        m += 3 * dataArray.length;
-                        h += Math.floor(m / 60);
-                        m %= 60;
-                        console.log('duration (3 min / benef) : ' + h + 'h ' + m);
-
-                      }
+                    ad.addBeneficiary({
+                      name: rows[0].name,
+                      birthdate: rows[0].birthdate,
+                      address_additional: rows[0].address_additional
                     });
+
+                    notInArray = false;
+                    ++addressesOffset;
+                    break;
                   }
-
-                  requestToOsrm('route');
-                  requestToOsrm('trip');
-
-
                 }
 
+                if (notInArray) {
+                  addresses[i - addressesOffset] = new AddressFeature(rows[0]);
 
-              });
-          }
+                  addresses[i - addressesOffset].addBeneficiary({
+                    name: rows[0].name,
+                    birthdate: rows[0].birthdate,
+                    address_additional: rows[0].address_additional
+                  });
+                }
+              }
 
-        });
+              if (++queriesDone == names.length) {
+                dbCon.end();
 
-    }
+                db.ccasAddress()
+                  .then((ccasAddress) => {
 
+                    addresses.unshift(new AddressFeature(ccasAddress));
+
+                    console.log('');
+                    console.log(addresses.length + ' addresses');
+                    // console.log(addresses);
+
+                    addresses = new FeatureCollection(addresses);
+
+                    // for(let adF of addresses.features)
+                    //   console.log(adF.properties);
+
+                    if (notFound == 0) {
+
+                      let testTrips = {
+                        original: {},
+                        osrmTrip: {},
+                        filled: 0,
+                        addresses: addresses
+                      };
+
+                      function requestToOsrm(service) {
+                        let oReq = new osrm.OsrmRequest(service, true);
+
+                        oReq.setFromAddresses(addresses);
+
+                        let madeUrl = oReq.makeUrl();
+
+
+                        request(madeUrl, (error, response, body) => {
+
+                          if (error) {
+                            console.log('error:', error); // Print the error if one occurred
+                            console.log('statusCode:', response.statusCode); // Print the response status code if a response was received
+                          } else {
+
+                            // console.log('response from ' + service + ' service');
+                            let parsedBody = JSON.parse(body);
+
+                            // l'objet route retourne par osrm, nom different selon le service
+                            let route = {};
+
+                            if (service == 'route') {
+                              testTrips.original = parsedBody;
+                              route = parsedBody.routes[0];
+
+                            } else if (service == 'trip') {
+
+                              testTrips.osrmTrip = parsedBody;
+                              route = parsedBody.trips[0];
+                            }
+
+                            if (++testTrips.filled == 2) {
+
+                              resolve(testTrips);
+                            }
+
+                            console.log('');
+                            console.log('** ' + service + ' service **');
+                            console.log('distance : ' + Math.ceil(route.distance / 10) / 100 +
+                              ' km');
+
+                            let h = Math.floor(route.duration / 3600);
+                            let m = Math.ceil((route.duration % 3600) / 60);
+                            console.log('duration : ' + h + 'h ' + m);
+
+
+                            m += 3 * addresses.features.length;
+                            h += Math.floor(m / 60);
+                            m %= 60;
+                            console.log('duration (3 min / address) : ' + h + 'h ' + m);
+
+                          }
+                        });
+                      }
+
+                      requestToOsrm('route');
+                      requestToOsrm('trip');
+
+
+                    }
+
+
+                  });
+              }
+
+            });
+
+        }
+
+
+
+      });
 
 
   });
