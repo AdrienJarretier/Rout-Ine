cedde145ef1ac595a3e6cf8fa649e5fdae31327a
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-05-24 14:31:58 +0200

    function to convert the one result file to multiple files for tablet download

---
+++ server/multipleFile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const common = require('./common.js');
+
+const TARGET_DIRECTORY = 'tests';
+
+common.readFile('gaResults/bestTours.json')
+  .then((content) => {
+
+    let object = JSON.parse(content);
+
+    console.log(object);
+
+    for (let i in object.trips) {
+
+      let tour = object.trips[i];
+
+      common.writeFile(TARGET_DIRECTORY + '/tourTrip' + i + '.js', 'let jsonFeatureTrip = ' + JSON.stringify(
+        tour.trip.trips[0], null, 2) + ';');
+
+      // common.writeJson(TARGET_DIRECTORY+'/tourTrip' + i + '.json', tour.trip.trips[0]);
+
+      for (let j in tour.addresses.features) {
+
+        tour.addresses.features[j].properties.waypoint_index = tour.trip.waypoints[j].waypoint_index;
+
+      }
+
+      // for (let j in tour.trip.trips[0]) {
+
+      //   trips[i].addresses.features[j].setWaypointIndex(trips[i].osrmTrip.waypoints[j].waypoint_index);
+
+      // }
+
+
+      common.writeFile(TARGET_DIRECTORY + '/tourAddresses' + i + '.js', 'let geojsonFeature = ' + JSON.stringify(
+        tour.addresses, null, 2) + ';');
+      // common.writeJson('tourAddresses' + i + '.json', tour.addresses);
+
+    }
+
+  });
