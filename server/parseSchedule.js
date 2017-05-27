290e039dc0363362c80b988efd32d81c11532ac8
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-05-27 21:51:18 +0200

    parsing schedule and creating an object listing beneficiaries

--- server/parseSchedule.js
+++ server/parseSchedule.js
@@ -1,18 +1,84 @@
 const csvParse = require('csv-parse');
 const common = require('./common.js');
 
-common.readFile('exampleTours/tournées_CCAS_par_dateShort.csv', 'binary')
-  .then((fileContent) => {
+
+
+class Address {
+
+  constructor(row) {
+
+    this.label = row[1];
+    this.town = row[3];
+
+  }
+
+}
+
+class Beneficiary {
+
+  constructor(row) {
+
+    this.address = new Address(row);
+    this.address_additional = row[2];
+    this.phones = [row[4]];
+    this.deliveries = [row[5]];
+    this.note = row[6];
+
+  }
+
+  addDelivery(row) {
+
+    this.deliveries.push(row[5]);
+
+  }
+
+}
+
+class BeneficiariesList {
+
+  constructor(parsedSchedule) {
+
+    this.beneficiaries = {};
+
+    for (let row of parsedSchedule) {
+
+      let benef = this.beneficiaries[row[0]];
+
+      if (benef)
+        benef.addDelivery(row);
+      else
+        this.beneficiaries[row[0]] = new Beneficiary(row);
+
+    }
+
+  }
+
+}
+
+function parseSchedule(schedule) {
+
+  return new Promise((resolve, reject) => {
 
     let options = {
       delimiter: ";",
       from: 2
     }
 
-    csvParse(fileContent, options, function(err, output) {
+    csvParse(schedule, options, function(err, output) {
 
-      console.log(output);
+      resolve(new BeneficiariesList(output));
 
     });
+
+  });
+}
+
+// this.export = parseSchedule;
+
+common.readFile('exampleTours/tournées_CCAS_par_dateShort.csv', 'binary')
+  .then(parseSchedule)
+  .then((output) => {
+
+    console.log(JSON.stringify(output, null, 2));
 
   });
