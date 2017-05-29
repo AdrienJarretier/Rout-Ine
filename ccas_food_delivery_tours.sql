8b9e90f6734b3dc10d41cb85b4b00b4b9e508de6
Jarretier Adrien <jarretier.adrien@gmail.com>
2017-05-29 02:00:26 +0200

    new database creation script

--- ccas_food_delivery_tours.sql
+++ ccas_food_delivery_tours.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 26, 2017 at 04:11 PM
--- Server version: 5.7.17-0ubuntu0.16.04.1
--- PHP Version: 7.0.15-0ubuntu0.16.04.4
+-- Host: 127.0.0.1
+-- Generation Time: May 28, 2017 at 11:59 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,10 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ccas_beneficiaries`
+-- Database: `ccas_food_delivery_tours`
 --
-CREATE DATABASE IF NOT EXISTS `ccas_beneficiaries` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `ccas_beneficiaries`;
+CREATE DATABASE IF NOT EXISTS `ccas_food_delivery_tours` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ccas_food_delivery_tours`;
 
 -- --------------------------------------------------------
 
@@ -35,19 +35,6 @@ CREATE TABLE `address` (
   `lat` double NOT NULL,
   `lng` double NOT NULL,
   `special` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `beneficiariesInTour`
---
-
-CREATE TABLE `beneficiariesInTour` (
-  `beneficiary_id` bigint(20) UNSIGNED NOT NULL,
-  `tour_num` tinyint(3) UNSIGNED NOT NULL,
-  `tour_date` date NOT NULL,
-  `indexInTour` smallint(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -69,6 +56,17 @@ CREATE TABLE `beneficiary` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `beneficiary_delivery_date`
+--
+
+CREATE TABLE `beneficiary_delivery_date` (
+  `beneficiary_id` bigint(20) UNSIGNED NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `beneficiary_phone`
 --
 
@@ -84,8 +82,19 @@ CREATE TABLE `beneficiary_phone` (
 --
 
 CREATE TABLE `tour` (
-  `num` tinyint(3) UNSIGNED NOT NULL,
-  `date` date NOT NULL
+  `num` tinyint(3) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tour_assignment`
+--
+
+CREATE TABLE `tour_assignment` (
+  `address_id` bigint(20) UNSIGNED NOT NULL,
+  `tour_num` tinyint(3) UNSIGNED NOT NULL,
+  `index_in_tour` smallint(5) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -100,19 +109,18 @@ ALTER TABLE `address`
   ADD UNIQUE KEY `label` (`label`,`town`);
 
 --
--- Indexes for table `beneficiariesInTour`
---
-ALTER TABLE `beneficiariesInTour`
-  ADD KEY `beneficiary_id_index` (`beneficiary_id`) USING BTREE,
-  ADD KEY `tour_keys_index` (`tour_num`,`tour_date`) USING BTREE;
-
---
 -- Indexes for table `beneficiary`
 --
 ALTER TABLE `beneficiary`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `unique_name_address` (`name`,`address_additional`,`address_id`),
   ADD KEY `address_id` (`address_id`);
+
+--
+-- Indexes for table `beneficiary_delivery_date`
+--
+ALTER TABLE `beneficiary_delivery_date`
+  ADD UNIQUE KEY `beneficiary_id` (`beneficiary_id`,`date`);
 
 --
 -- Indexes for table `beneficiary_phone`
@@ -124,7 +132,14 @@ ALTER TABLE `beneficiary_phone`
 -- Indexes for table `tour`
 --
 ALTER TABLE `tour`
-  ADD PRIMARY KEY (`num`,`date`);
+  ADD PRIMARY KEY (`num`);
+
+--
+-- Indexes for table `tour_assignment`
+--
+ALTER TABLE `tour_assignment`
+  ADD UNIQUE KEY `tour_num` (`tour_num`,`index_in_tour`),
+  ADD UNIQUE KEY `address_id` (`address_id`) USING BTREE;
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -134,22 +149,15 @@ ALTER TABLE `tour`
 -- AUTO_INCREMENT for table `address`
 --
 ALTER TABLE `address`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=589;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `beneficiary`
 --
 ALTER TABLE `beneficiary`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=693;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `beneficiariesInTour`
---
-ALTER TABLE `beneficiariesInTour`
-  ADD CONSTRAINT `beneficiariesInTour_ibfk_1` FOREIGN KEY (`tour_num`,`tour_date`) REFERENCES `tour` (`num`, `date`),
-  ADD CONSTRAINT `beneficiariesInTour_ibfk_2` FOREIGN KEY (`beneficiary_id`) REFERENCES `beneficiary` (`id`);
 
 --
 -- Constraints for table `beneficiary`
@@ -158,10 +166,23 @@ ALTER TABLE `beneficiary`
   ADD CONSTRAINT `fk_address_id` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`);
 
 --
+-- Constraints for table `beneficiary_delivery_date`
+--
+ALTER TABLE `beneficiary_delivery_date`
+  ADD CONSTRAINT `beneficiary_delivery_date_ibfk_1` FOREIGN KEY (`beneficiary_id`) REFERENCES `beneficiary` (`id`);
+
+--
 -- Constraints for table `beneficiary_phone`
 --
 ALTER TABLE `beneficiary_phone`
   ADD CONSTRAINT `fk_beneficiary_id` FOREIGN KEY (`beneficiary_id`) REFERENCES `beneficiary` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tour_assignment`
+--
+ALTER TABLE `tour_assignment`
+  ADD CONSTRAINT `tour_assignment_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`),
+  ADD CONSTRAINT `tour_assignment_ibfk_2` FOREIGN KEY (`tour_num`) REFERENCES `tour` (`num`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
